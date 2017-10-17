1d73e82f387a7640eadd422532f0c8e9c0e0f5c5
Jason Witherspoon <jasonwit@usc.edu>
2017-10-16 20:56:50 -0700

    Update populate_DB.sql

--- populate_DB.sql
+++ populate_DB.sql
@@ -1,18 +1,18 @@
 #Create Users 
 INSERT INTO Users(fName, lName, email, password)
-VALUES("Bob", "Muller", "bmuller@usc.edu", "password1");
+VALUES("Bob", "Muller", "bmuller@usc.edu", "7c6a180b36896a0a8c02787eeafb0e4c");
 
 INSERT INTO Users(fName, lName, email, password)
-VALUES("Natalie", "Monger", "nmonger@usc.edu", "password2"); 
+VALUES("Natalie", "Monger", "nmonger@usc.edu", "f1f16683f3e0208131b46d37a79c8921"); 
 
 INSERT INTO Users(fName, lName, email, password)
-VALUES("Morgan", "Bent", "mbent@usc.edu", "password3"); 
+VALUES("Morgan", "Bent", "mbent@usc.edu", "1349b4563dc7254f815c1c7609d39efe"); 
 
 INSERT INTO Users(fName, lName, email, password)
-VALUES("Peter" , "Tillman", "ptillman@usc.edu", "password4"); 
+VALUES("Peter" , "Tillman", "ptillman@usc.edu", "3bb8a650ecae07903d2dc68dc72c9a3c"); 
 
 INSERT INTO Users(fName, lName, email, password)
-VALUES("Sebastian" , "Rinkema", "srinkema@usc.edu", "password5"); 
+VALUES("Sebastian" , "Rinkema", "srinkema@usc.edu", "4b85107bcceddab21e05f262680d2b7f"); 
 
 
 
@@ -46,102 +46,100 @@ VALUES(4, 1, "Design for America USC", 0);
 
 #Create Pizza Entities
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(1, "Pizza Studio", "Pizza in a studio", 0, "https://pbs.twimg.com/profile_images/515257364681728001/Hk7MD3Cb.png", 0, 1, '2019-10-10 23:59:59');
+VALUES(2, "Pizza Studio", "Pizza in a studio", 0, "https://pbs.twimg.com/profile_images/515257364681728001/Hk7MD3Cb.png", 0, 1, '2019-10-10 23:59:59');
 
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(2, "Blaze Pizza", "Pizza that is Blazin' ", 0, "https://s3-media4.fl.yelpcdn.com/bphoto/_xLCa8wyxWUygxHJvkodNA/ls.jpg", 0, 1, '2019-04-10 23:59:59');
+VALUES(3, "Blaze Pizza", "Pizza that is Blazin' ", 0, "https://s3-media4.fl.yelpcdn.com/bphoto/_xLCa8wyxWUygxHJvkodNA/ls.jpg", 0, 1, '2019-04-10 23:59:59');
 
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(3, "Domino's",  "Pizza that is good", 0, "https://cache.dominos.com/olo/5_14_2/assets/build/images/promo/dominos_social_logo.jpg", 0, 1, '2019-04-05 23:59:59');
+VALUES(4, "Domino's",  "Pizza that is good", 0, "https://cache.dominos.com/olo/5_14_2/assets/build/images/promo/dominos_social_logo.jpg", 0, 1, '2019-04-05 23:59:59');
 
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(4, "Pizza Hut", "Pizza with the Hut", 0, "https://upload.wikimedia.org/wikipedia/az/thumb/9/99/Pizza_Hut.svg/1200px-Pizza_Hut.svg.png", 0, 1, '2019-08-05 23:59:59');
+VALUES(5, "Pizza Hut", "Pizza with the Hut", 0, "https://upload.wikimedia.org/wikipedia/az/thumb/9/99/Pizza_Hut.svg/1200px-Pizza_Hut.svg.png", 0, 1, '2019-08-05 23:59:59');
 
 
 #Create Pizza Entity Tags 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(1, 1, "Pizza");
+VALUES(3, 2, "Pizza");
 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(2 ,2, "Pizza");
+VALUES(4 ,3, "Pizza");
 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(3, 3, "Pizza");
+VALUES(5, 4, "Pizza");
 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(4, 4, "Pizza");
+VALUES(6, 5, "Pizza");
 
 
 
 #Create Computer Science Entities
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(5, "CS 104",  "The worst class, hands down", 0, "http://www-bcf.usc.edu/~aaroncot/aaron.jpg", 0, 0, '2019-04-06 23:59:59');
+VALUES(6, "CS 104",  "The worst class, hands down", 0, "http://www-bcf.usc.edu/~aaroncot/aaron.jpg", 0, 0, '2019-04-06 23:59:59');
 
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(6, "CS 170", "Not as bad as 104", 0, "http://www-bcf.usc.edu/~aaroncot/aaron.jpg", 0, 1, '2019-04-03 23:59:59');
+VALUES(7, "CS 170", "Not as bad as 104", 0, "http://www-bcf.usc.edu/~aaroncot/aaron.jpg", 0, 1, '2019-04-03 23:59:59');
 
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(7, "CS 201", "Not too bad",  0, "https://viterbi.usc.edu/directory/images/2dafc9bc59c736884ae57b7235d5bc67.jpg", 0, 1, '2019-04-06 23:59:59');
+VALUES(8, "CS 201", "Not too bad",  0, "https://viterbi.usc.edu/directory/images/2dafc9bc59c736884ae57b7235d5bc67.jpg", 0, 1, '2019-04-06 23:59:59');
 
 INSERT INTO Entities(entityID, title, description, rating, image, numViews, isInfinite, timeEnd)
-VALUES(8, "CS 270", "Makes 170 look like a joke", 0, "http://www-bcf.usc.edu/~aaroncot/aaron.jpg", 0, 1, '2019-01-03 23:59:59');
+VALUES(9, "CS 270", "Makes 170 look like a joke", 0, "http://www-bcf.usc.edu/~aaroncot/aaron.jpg", 0, 1, '2019-01-03 23:59:59');
 
 
 #Create Computer Science Entity Tags 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(5, 5,  "Computer Science");
+VALUES(7, 6,  "Computer Science");
 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(6, 6,  "Computer Science");
+VALUES(8, 7,  "Computer Science");
 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(7, 7,  "Computer Science");
+VALUES(9, 8,  "Computer Science");
 
 INSERT INTO EntityTags(tagID, entityID, title)
-VALUES(8, 8, "Computer Science");
+VALUES(10, 9, "Computer Science");
 
 #Create Computer Science Poll 
 INSERT INTO Polls(pollID, title, image, numViews, isInfinite, timeEnd )
-VALUES(2, "Best Computer Science Class", "http://www.austincc.edu/sites/default/files/Computer-Science_10282016.jpg ", 0, 1, '2019-02-03 23:59:59');
+VALUES(10, "Best Computer Science Class", "http://www.austincc.edu/sites/default/files/Computer-Science_10282016.jpg ", 0, 1, '2019-02-03 23:59:59');
 
 #Create Computer Science Poll Tags 
 INSERT INTO PollTags(tagID, pollID, title)
-VALUES(3, 2 , "Computer Science");
+VALUES(11, 10 , "Computer Science");
 
 #Create Computer Science Poll Options 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(5, 2, "CS 104", 0);
+VALUES(5, 10, "CS 104", 0);
 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(6, 2, "CS 170", 0);
+VALUES(6, 10, "CS 170", 0);
 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(7, 2, "CS 201", 0);
+VALUES(7, 10, "CS 201", 0);
 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(8, 2, "CS 270", 0);
+VALUES(8, 10, "CS 270", 0);
 
 
 #Create Pizza Poll 
 INSERT INTO Polls(pollID, title, image, numViews, isInfinite, timeEnd)
-VALUES(3, "Best Pizza at USC", "https://www.cicis.com/media/1138/pizza_trad_pepperoni.png", 0, 0, '2019-04-05 23:59:59'); 
+VALUES(11, "Best Pizza at USC", "https://www.cicis.com/media/1138/pizza_trad_pepperoni.png", 0, 0, '2019-04-05 23:59:59'); 
 
 #Create Pizza Poll Tags
 INSERT INTO PollTags(tagID, pollID, title) 
-VALUES(4, 3, "Pizza");
+VALUES(12, 11, "Pizza");
 
-INSERT INTO PollTags(tagID, pollID, title) 
-VALUES(5, 3, "Food"); 
 
 #Create Pizza Poll Options
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(9, 3, "Pizza Studio", 0);
+VALUES(9, 11, "Pizza Studio", 0);
 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(10, 3, "Blaze Pizza", 0);
+VALUES(10, 11, "Blaze Pizza", 0);
 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(11, 3, "Dominos", 0);
+VALUES(11, 11, "Dominos", 0);
 
 INSERT INTO Options(optionID, pollID, title, numVotes)
-VALUES(12, 3, "Pizza Hut", 0);
+VALUES(12, 11, "Pizza Hut", 0);
