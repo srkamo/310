3af9cacbbed6956d0246d27f1cdc4014073e5191
Jason Witherspoon <jasonwit@usc.edu>
2017-10-29 11:10:59 -0700

    Add files via upload

---
+++ SeleniumPollTest.java
@@ -0,0 +1,90 @@
+package testing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class SeleniumPollTest {
+	@Test 
+	public void testUserMustBeLoggedIn(){
+		WebDriver browser;   
+
+		//Firefox's gecko-driver requires you to specify its location.    
+		System.setProperty("webdriver.gecko.driver","/Users/jasonwitherspoon/Desktop/geckodriver");    
+
+		browser = new FirefoxDriver();    
+		browser.get("http://localhost:8080/KnowItAll/index.jsp");   
+		
+		browser.get("http://localhost:8080/KnowItAll/login.jsp"); 
+	  
+		browser.get("http://localhost:8080/KnowItAll/pollPage.jsp?subjectID=11"); 
+		
+		WebElement submitVoteForm = browser.findElement(By.className("iradio_square-yellow"));
+	    submitVoteForm.submit();  
+
+	    //check the browser -- there should be an error message on the poll page saying that 
+	    //the user has already voted on this poll
+	}
+
+	
+	@Test 
+	public void loggedInPollVote() {
+		WebDriver browser;   
+		System.setProperty("webdriver.gecko.driver","/Users/jasonwitherspoon/Desktop/geckodriver");    
+		
+		browser = new FirefoxDriver();    
+		browser.get("http://localhost:8080/KnowItAll/index.jsp");   
+		
+		browser.get("http://localhost:8080/KnowItAll/login.jsp"); 
+		
+    		WebElement signinform = browser.findElement(By.name("signinform"));
+    		WebElement usernameElement = browser.findElement(By.name("l_email"));
+		WebElement passwordElement = browser.findElement(By.name("l_password"));
+	
+		usernameElement.sendKeys("mbent@usc.edu");
+    		passwordElement.sendKeys("password3");
+	
+    		signinform.submit(); 
+    		
+    		browser.get("http://localhost:8080/KnowItAll/pollPage.jsp?subjectID=11"); 
+
+    		WebElement submitVoteForm = browser.findElement(By.className("iradio_square-yellow"));
+    	    submitVoteForm.submit(); 
+	}
+	
+	@Test 
+	public void testUserCanOnlyVoteOnce(){
+		WebDriver browser;   
+		System.setProperty("webdriver.gecko.driver","/Users/jasonwitherspoon/Desktop/geckodriver");    
+		
+		browser = new FirefoxDriver();    
+		browser.get("http://localhost:8080/KnowItAll/index.jsp");   
+		
+		browser.get("http://localhost:8080/KnowItAll/login.jsp"); 
+		
+    		WebElement signinform = browser.findElement(By.name("signinform"));
+    		WebElement usernameElement = browser.findElement(By.name("l_email"));
+		WebElement passwordElement = browser.findElement(By.name("l_password"));
+	
+		usernameElement.sendKeys("mbent@usc.edu");
+    		passwordElement.sendKeys("password3");
+	
+    		signinform.submit(); 
+    		
+    		browser.get("http://localhost:8080/KnowItAll/pollPage.jsp?subjectID=11"); 
+
+    		WebElement submitVoteForm = browser.findElement(By.className("iradio_square-yellow"));
+    	    submitVoteForm.submit();  
+    	    
+    		browser.get("http://localhost:8080/KnowItAll/pollPage.jsp?subjectID=11"); 
+
+    	    WebElement submitVoteForm2 = browser.findElement(By.className("iradio_square-yellow"));
+    	    submitVoteForm.submit();  
+	}
+
+}
