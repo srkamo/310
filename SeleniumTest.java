893b89ed2603fd01f01431c1895c174925b7c9fd
Morgan Bent <mbent@usc.edu>
2017-10-29 10:46:45 -0700

    Add files via upload
    Morgan's final black box test cases

--- SeleniumTest.java
+++ SeleniumTest.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTest {
 
-	//@Test
+	@Test
 	public void testPollPageScoresAndComments() {
 		WebDriver browser;
 		
@@ -29,7 +29,7 @@ public class SeleniumTest {
 		browser.close();
 	}
 	
-	//@Test
+	@Test
 	public void testEntityPageRatingAndComments(){
 		WebDriver browser;
 		
@@ -74,6 +74,57 @@ public class SeleniumTest {
 	    submitVoteForm.submit(); 
 	    WebElement submitVoteForm2 = browser.findElement(By.name("submitVoteForm"));
 	    submitVoteForm2.submit(); 
+
+	    //check the browser -- there shoud be an error message on the poll page saying that 
+	    //the user has already voted on this poll
+	}
+	
+	@Test 
+	public void testCreatePollPage(){
+		WebDriver browser;   
+
+		//Firefox's geckodriver requires you to specify its location.    
+		System.setProperty("webdriver.gecko.driver", "/Users/morganbent/Downloads/geckodriver");   
+
+		browser = new FirefoxDriver();    
+		browser.get("http://localhost:8080/KnowItAll/index.jsp");   
+		
+		browser.get("http://localhost:8080/KnowItAll/createPollPage.jsp"); 
+		
+		assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/KnowItAll/createPollPage.jsp"));
+		browser.close();
+	}
+	
+	@Test
+	public void testUserPageFeedUpdates(){
+		WebDriver browser;   
+
+		//Firefox's geckodriver requires you to specify its location.    
+		System.setProperty("webdriver.gecko.driver", "/Users/morganbent/Downloads/geckodriver");   
+
+		browser = new FirefoxDriver();    
+		browser.get("http://localhost:8080/KnowItAll/index.jsp");   
+		
+		browser.get("http://localhost:8080/KnowItAll/login.jsp"); 
+	    
+	    WebElement signinform = browser.findElement(By.name("signinform"));
+	    WebElement usernameElement = browser.findElement(By.name("l_email"));
+		WebElement passwordElement = browser.findElement(By.name("l_password"));
+		
+		usernameElement.sendKeys("mbent@usc.edu");
+	    passwordElement.sendKeys("password3");
+		
+	    signinform.submit(); 
+
+		browser.get("http://localhost:8080/KnowItAll/pollPage.jsp?subjectID=11"); 
+		
+		WebElement submitVoteForm = browser.findElement(By.name("submitVoteForm"));
+	    submitVoteForm.submit(); 
+	   
+	    browser.get("http://localhost:8080/KnowItAll/userPage.jsp"); 
+	    WebElement historyElement = browser.findElement(By.id("userHistory"));
+	    
+	    assertTrue(historyElement.isDisplayed());
 
 	    //check the browser -- there shoud be an error message on the poll page saying that 
 	    //the user has already voted on this poll
